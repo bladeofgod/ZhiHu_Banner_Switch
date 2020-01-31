2a7fcd45a4810b02835334801d3ee37cff624abb
bladeofgod <450103187@qq.com>
2020-01-31 11:57:30 +0800

    1.0

---
+++ app/src/main/java/com/bedrock/zhihu_banner_switch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bedrock.zhihu_banner_switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

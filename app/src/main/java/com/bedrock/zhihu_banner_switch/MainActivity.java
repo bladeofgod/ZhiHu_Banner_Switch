2c6e194111eeb2b30140c298fe85ebf94ec6d802
bladeofgod <450103187@qq.com>
2020-01-31 21:29:07 +0800

    develop

--- app/src/main/java/com/bedrock/zhihu_banner_switch/MainActivity.java
+++ app/src/main/java/com/bedrock/zhihu_banner_switch/MainActivity.java
@@ -2,7 +2,9 @@ package com.bedrock.zhihu_banner_switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private MyScrollView scrollView;
     private ImageView ivPurple,ivOrange;
+
+    private boolean isScrollUp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +31,39 @@ public class MainActivity extends AppCompatActivity {
         ivOrange = findViewById(R.id.iv_orange);
         ivPurple = findViewById(R.id.iv_purple);
 
+        ivPurple.setDrawingCacheEnabled(true);
+        ivOrange.setDrawingCacheEnabled(true);
+
+        bmPurple = ivPurple.getDrawingCache();
+        bmOrange = ivOrange.getDrawingCache();
+
+        ivPurple.setDrawingCacheEnabled(false);
+        ivOrange.setDrawingCacheEnabled(false);
+
+
         scrollView = findViewById(R.id.scroll_view);
     }
+    private Bitmap bmPurple,bmOrange;
 
     private void initListener(){
         scrollView.setListener(new MyScrollView.onScrollListener() {
             @Override
             public void onScroll(int l, int t, int oldl, int oldt) {
+                Log.i("scrollview",
+                        "l : " + l + "   t :" + t + "   oldl :" + oldl+ "  oldt: " + oldt);
+                if(t > oldt){
+                    //scroll down
+                    isScrollUp = false;
+                }else{
+                    isScrollUp = true;
+                }
 
             }
         });
     }
+
+    //只需要裁剪上层 也就是 iv orange
+    private void cropBitmap(){}
 
 
 }

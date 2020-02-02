90ab5eb57924b3c6620aa51f6e656efc3405dc3b
bladeofgod <450103187@qq.com>
2020-02-02 13:05:03 +0800

    test

--- app/src/main/java/com/bedrock/zhihu_banner_switch/MainActivity.java
+++ app/src/main/java/com/bedrock/zhihu_banner_switch/MainActivity.java
@@ -3,8 +3,18 @@ package com.bedrock.zhihu_banner_switch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 
@@ -28,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView(){
+//        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+//        screenWidth = wm.getDefaultDisplay().getWidth();
+        WindowManager wm = getWindowManager();
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        screenWidth = dm.widthPixels;
+
         ivOrange = findViewById(R.id.iv_orange);
         ivPurple = findViewById(R.id.iv_purple);
 
@@ -42,7 +59,18 @@ public class MainActivity extends AppCompatActivity {
 
 
         scrollView = findViewById(R.id.scroll_view);
+
+        findViewById(R.id.btn_crop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ivOrange.setImageBitmap(cropBitmapInCircleWay(bmOrange,screenWidth/ratio));
+                ratio ++;
+            }
+        });
+
     }
+    private int ratio = 2;
+    private int screenWidth;
     private Bitmap bmPurple,bmOrange;
 
     private void initListener(){
@@ -63,7 +91,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //只需要裁剪上层 也就是 iv orange
-    private void cropBitmap(){}
+    //
+
+    /**
+     * @param origin 待裁剪
+     * @param radius 圆形半径
+     *
+     *  从原图（矩形）左上角开始为圆心，进行裁剪，
+     *  不断扩大半径
+     */
+
+    private Bitmap cropBitmapInCircleWay(Bitmap origin,float radius){
+        if(origin == null){
+            return null;
+        }
+
+        //创建一个原图 备份
+        Bitmap circleBitmap = Bitmap.createBitmap(origin.getWidth(), origin.getHeight(),
+                Bitmap.Config.RGB_565);
+        try {
+
+            //画布
+            Canvas canvas = new Canvas(circleBitmap);
+            final Paint paint = new Paint();
+            final Rect rect = new Rect(0,0,origin.getWidth(),origin.getHeight());
+            final RectF rectF = new RectF(new Rect(0,0,origin.getWidth(),origin.getHeight()));
+
+            paint.setAntiAlias(true);
+            //设置画布模式
+            canvas.drawARGB(0,0,0,0);
+            paint.setColor(Color.WHITE);
+            canvas.drawRoundRect(rectF, radius, radius, paint);
+            //遮罩模式  具体可以参考这个网址
+            //https://www.cnblogs.com/tianzhijiexian/p/4297172.html
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            final Rect src = new Rect(0,0,(int)radius,(int)radius);
+            canvas.drawBitmap(origin, src,rect,paint);
+            return circleBitmap;
+
+        }catch (Exception e){
+            return origin;
+        }
+
+    }
 
 
 }
